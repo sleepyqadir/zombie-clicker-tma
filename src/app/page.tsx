1eeea1ac1fa615e569a19c507c167ba6ea3c75c0
sleepyqadir <a.qadir67@outlook.com>
2024-11-28 15:22:40 +0500

    fix: balance component margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <main className="p-4 pb-10 max-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
-        <div className="flex justify-center mb-40">
+        <div className="flex justify-center mb-10">
           {address ? (
             <>
               <Button
